58977f84d4f0fa78361fd9e4d9234cc33a454b9a
LinkStatu <murtmoh1993@gmail.com>
2023-08-20 14:59:57 +0300

    Implemnting some edits

---
+++ app/src/main/java/com/link/statu/activities/MoreWallsActivity.java
@@ -0,0 +1,78 @@
+package com.link.statu.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentManager;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.link.statu.R;
+import com.link.statu.data_source.DataService;
+import com.link.statu.fragments.WallsFragment;
+
+public class MoreWallsActivity extends AppCompatActivity {
+
+    private WallsFragment wallsFragment;
+    private AdView adView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_more_walls);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setNavigationOnClickListener(view -> finish());
+
+        initAd();
+
+        Intent intent = getIntent();
+        FragmentManager manager = getSupportFragmentManager();
+        wallsFragment = (WallsFragment) manager.findFragmentById(R.id.moreFragment);
+        if (intent.hasExtra("category")) {
+            String category = intent.getStringExtra("category");
+            toolbar.setTitle(category);
+            assert wallsFragment != null;
+            wallsFragment.setFragment(DataService.QueryType.CATEGORY, category);
+        } else {
+            finish();
+        }
+
+    }
+
+    private void initAd() {
+        MobileAds.initialize(this, initializationStatus -> {
+        });
+
+        adView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
+    }
+
+    @Override
+    protected void onStart() {
+        wallsFragment.focus();
+        super.onStart();
+    }
+
+    @Override
+    protected void onPause() {
+        adView.pause();
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        adView.resume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        adView.destroy();
+        super.onDestroy();
+    }
+}
